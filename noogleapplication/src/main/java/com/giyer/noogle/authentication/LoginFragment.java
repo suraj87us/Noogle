80fd53b422e83924efbb2f85d31d5a879b602928
Gopalakrishnan Iyer <gopalakrishnan.iyer@aexp.com>
2017-03-07 22:17:51 -0500

    Adding skeleton Activities and Fragments

---
+++ noogleapplication/src/main/java/com/giyer/noogle/authentication/LoginFragment.java
@@ -0,0 +1,8 @@
+package com.giyer.noogle.authentication;
+
+/**
+ * Created by giyer7 on 3/7/17.
+ */
+
+public class LoginFragment {
+}
---
+++ noogleapplication/src/main/java/com/giyer/noogle/base/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.giyer.noogle.base;
+
+import android.app.Fragment;
+
+/**
+ * Created by giyer7 on 3/7/17.
+ */
+
+public class BaseFragment extends Fragment {
+}
